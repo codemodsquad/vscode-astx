a74575d660b144a30eb6992d65d9e3163af44143
Andy Edwards <jedwards@fastmail.com>
2022-09-07 17:29:56 -0500

    chore: copy webview-view-sample code

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import { AstxViewProvider } from './AstxViewProvider'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -27,6 +28,14 @@ export function activate(context: vscode.ExtensionContext): void {
   )
 
   context.subscriptions.push(disposable)
+
+  const provider = new AstxViewProvider(context.extensionUri)
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      AstxViewProvider.viewType,
+      provider
+    )
+  )
 }
 
 // this method is called when your extension is deactivated
